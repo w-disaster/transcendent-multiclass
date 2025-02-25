ad1c78ae07cfddf8fcc26444a0996757bc4805bd
w-disaster <luca.fabri1999@gmail.com>
2025-02-25 14:45:20 +0000

    refactor: remove useless files

--- run.sh
+++
@@ -1,9 +0,0 @@
-
-source venv/bin/activate
-python3 ice.py	                  	    \
-    -k 10                       	    \
-    -n 10                       	    \
-    --pval-consider full-train  	    \
-    -t quartiles                	    \
-    --q-consider correct              \
-    -c cred
