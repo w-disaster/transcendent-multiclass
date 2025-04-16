7a26b6a6e66db31e07dde0bdddc69550ee98c4bf
w-disaster <luca.fabri1999@gmail.com>
2025-04-16 14:01:22 +0000

    feat: parallel RF proximities

--- transcend/classifiers/random_forest.py
+++ transcend/classifiers/random_forest.py
@@ -1,9 +1,14 @@
+from multiprocessing import Pool
+
 import numpy as np
-from tqdm import tqdm
 from sklearn.ensemble import RandomForestClassifier
-from multiprocessing import Pool, shared_memory
+from tqdm import tqdm
 from transcend.classifiers.ncm_classifier import NCMClassifier
-from transcend.utils import alloc_shm, close_and_unlink_shm, load_existing_shm, close_shm
+
+from transcend.utils import (
+    alloc_shm,
+    load_existing_shm,
+)
 
 eps = np.finfo(np.float64).eps
 
@@ -12,20 +17,59 @@ def parall_prox(args):
     (shm_tuple, (example_idx, end_idx)) = args
     (t_leaves_shm_t, leaves_shm_t) = shm_tuple
 
-    t_leaves = load_existing_shm(*t_leaves_shm_t)
-    leaves = load_existing_shm(*leaves_shm_t)
+    t_leaves_shm, t_leaves = load_existing_shm(*t_leaves_shm_t)
+    leaves_shm, leaves = load_existing_shm(*leaves_shm_t)
 
-    try:
-        proximity = np.mean(
-            leaves[..., np.newaxis]
-            == t_leaves[:, example_idx:end_idx][np.newaxis, ...],
-            axis=1,
-        )
-    finally:
-        close_shm(t_leaves_shm_t[0])
-        close_shm(leaves_shm_t[0])
-        
+    proximity = np.mean(
+        leaves[..., np.newaxis]
+        == t_leaves[:, example_idx:end_idx][np.newaxis, ...],
+        axis=1,
+    )
+
+    t_leaves_shm.close()
+    leaves_shm.close()
+    # close_shm(t_leaves_shm_t[0])
+    # close_shm(leaves_shm_t[0])
+
     return proximity
+
+
+def compute_proximity(X_train_len, train_leaves, leaves, step_size=100):
+    """
+    Computes the proximity between each pair of examples.
+    :param leaves: A matrix of shape [num_example, num_tree] where the value [i,j] is
+      the index of the leaf reached by example "i" in the tree "j".
+    :param step_size: Size of the block of examples for the computation of the
+      proximity. Does not impact the results.
+    :return: The example pair-wise proximity matrix of shape [n,n] with "n" the number of
+    examples.
+    """
+    num_examples = X_train_len
+    t_leaves = np.transpose(train_leaves)
+
+    # Instead of computing the proximity in between all the examples at the same
+    # time, we compute the similarity in blocks of "step_size" examples. This
+    # makes the code more efficient with the the numpy broadcast.
+    ex_end_idxs = [
+        (i, min(i + step_size, num_examples))
+        for i in range(0, num_examples, step_size)
+    ]
+
+    t_leaves_shm, t_leaves_shm_t = alloc_shm(t_leaves)
+    leaves_shm, leaves_shm_t = alloc_shm(leaves)
+
+    shm_tuple = (t_leaves_shm_t, leaves_shm_t)
+    with Pool(32) as p:
+        proximities = p.map(
+            parall_prox, [(shm_tuple, ex_end_idx) for ex_end_idx in ex_end_idxs]
+        )
+
+    t_leaves_shm.close()
+    t_leaves_shm.unlink()
+    leaves_shm.close()
+    leaves_shm.unlink()
+
+    return np.concatenate(proximities, axis=1)
 
 
 class RandomForestNCMClassifier(NCMClassifier, RandomForestClassifier):
@@ -37,71 +81,19 @@ class RandomForestNCMClassifier(NCMClassifier, RandomForestClassifier):
         self.X_train = None
         self.y_train = None
 
-    def __compute_proximity(self, leaves, step_size=100):
-        """
-        Computes the proximity between each pair of examples.
-        :param leaves: A matrix of shape [num_example, num_tree] where the value [i,j] is
-          the index of the leaf reached by example "i" in the tree "j".
-        :param step_size: Size of the block of examples for the computation of the
-          proximity. Does not impact the results.
-        :return: The example pair-wise proximity matrix of shape [n,n] with "n" the number of
-        examples.
-        """
-        example_idx = 0
-        num_examples = len(self.X_train)
-
-        t_leaves = np.transpose(self.train_leaves)
-        # proximities = []
-
-        # Instead of computing the proximity in between all the examples at the same
-        # time, we compute the similarity in blocks of "step_size" examples. This
-        # makes the code more efficient with the the numpy broadcast.
-
-        ex_end_idxs = []
-
-        while example_idx < num_examples:
-            end_idx = min(example_idx + step_size, num_examples)
-            ex_end_idxs.append((example_idx, end_idx))
-            example_idx = end_idx
-
-        t_leaves_shm_t = alloc_shm(t_leaves)
-        leaves_shm_t = alloc_shm(leaves)
-
-        shm_tuple = (t_leaves_shm_t, leaves_shm_t)
-        with Pool(32) as p:
-            try:
-                proximities = p.map(parall_prox, [(shm_tuple, ex_end_idx) for ex_end_idx in ex_end_idxs])
-            finally:
-                close_and_unlink_shm(t_leaves_shm_t[0])
-                close_and_unlink_shm(leaves_shm_t[0])
-
-        # train_ncms_shm.name, groundtruth_train.shape, train_ncms.dtype
-
-        # while example_idx < num_examples:
-        #         end_idx = min(example_idx + step_size, num_examples)
-        #         proximities.append(
-        #             np.mean(
-        #                 leaves[..., np.newaxis]
-        #                 == t_leaves[:, example_idx:end_idx][np.newaxis, ...],
-        #                 axis=1,
-        #             )
-        #         )
-        #         example_idx = end_idx
-        return np.concatenate(proximities, axis=1)
-
     def fit(self, X_train, y_train):
         super().fit(X_train, y_train)
         self.X_train = X_train
         self.y_train = y_train
         self.train_leaves = self.apply(X_train)
-        self.proximity = self.__compute_proximity(self.train_leaves)
+        self.proximity = compute_proximity(len(X_train), self.train_leaves, self.train_leaves)
 
     def ncm(self, X, y):
         if np.array_equal(X, self.X_train):
             proximity = self.proximity
         else:
             leaves = self.apply(X)
-            proximity = self.__compute_proximity(leaves)
+            proximity = compute_proximity(len(self.X_train), self.train_leaves, leaves)
 
         def single_ncm(i):
             avg_prox_diff = np.mean(np.sort(proximity[i, self.y_train != y[i]])[-5:])
