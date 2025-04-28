28b04cada7f13a92216a32f10bb837ffb924318d
w-disaster <luca.fabri1999@gmail.com>
2025-04-28 08:07:56 +0000

    chore: remove useless files

--- run-cred.sh
+++
@@ -1,8 +0,0 @@
-# run all cred experiments
-
-# these experiments produce plots for CCE, TCE, ICE
-# p-vals and probabilities using cred
-
-bash experiments/tce-ice-cce-cred/tce.sh &&
-bash experiments/tce-ice-cce-cred/ice.sh &&
-bash experiments/tce-ice-cce-cred/cce.sh &&
